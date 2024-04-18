e136867c881b8ce8dcab6cd2ba59b95f4df69e64
VitorManoelLP <vitor.m.lima.p@gmail.com>
2024-04-18 18:34:01 -0300

    revert auth

--- social-media/src/main/java/com/socialmedia/main/infra/configuration/SecurityConfiguration.java
+++ social-media/src/main/java/com/socialmedia/main/infra/configuration/SecurityConfiguration.java
@@ -3,10 +3,13 @@ package com.socialmedia.main.infra.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.Collection;
 import java.util.Map;
@@ -14,6 +17,14 @@ import java.util.stream.Collectors;
 
 @Configuration
 public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())))
+                .build();
+    }
 
     @Bean
     @SuppressWarnings("unchecked")
